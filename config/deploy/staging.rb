dd5f0e5863aaf1cd6d8279a959ad2d2296621a3f
Jakub Adler <adler.jakub@gmail.com>
2023-08-24 20:26:49 +0200

    chore: install ssh key namually use ssh-add

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -49,15 +49,15 @@ role :web, %w[fortyunbroken.com]
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
+server "example.com",
+  user: "user_name",
+  roles: %w{web app},
+  ssh_options: {
+    user: "user_name", # overrides user setting above
+    keys: %w(/home/user_name/.ssh/id_rsa),
+    forward_agent: false,
+    auth_methods: %w(publickey password)
+    # password: "please use keys"
+  }
 
 set :deploy_to, '/home/git/apps/forty-unbroken/staging'
