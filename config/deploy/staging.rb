78c525cb7842f6e80f10238cc48155288ad3a3d2
Jakub Adler <adler.jakub@gmail.com>
2023-09-01 08:54:16 +0200

    build: reset bundler --quiet option for staging env The build works bundler run successfully on remote server and migrations are run as well nest step run puma

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,3 +2,4 @@ set :deploy_to, '/home/git/apps/forty-unbroken/staging'
 set :rails_env, 'staging'
 set :migration_role, :app
 set :conditionally_migrate, true
+set :bundle_flags, '' # reset bundler flags for staging to see the progress
